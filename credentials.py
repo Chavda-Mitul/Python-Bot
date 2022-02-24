9c738132e19753fec8c59227ceff4be9b94aae85
Chavda-Mitul <82195661+Chavda-Mitul@users.noreply.github.com>
2022-02-24 23:44:38 +0530

    Add files via upload

---
+++ credentials.py
@@ -0,0 +1,8 @@
+# This is just a sample file. You need to
+# edit this file. You need to get these
+# details from your Twitter app settings.
+
+consumer_key = 'dnVMcUxjdkllZjdhYndFbWtrZm06MTpjaQ'
+consumer_secret = '5SJQDTJlwWraP8nW8PxnI3GsJX0qqVCeoe_2uQk4JS4wzetFMO'
+access_token = '1462026884124917760-PVpPJj0J4kI1V9brqsGBFFdAbMPEHC'
+access_token_secret = 'j44ajIeprpKkYKZg0AYPAxjttIrRMlI1WyitKI9F15Sfx'
